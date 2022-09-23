169726fa4ab79f2a2825ba8104605ad20d4c4cb8
KanSlavanO <SlaviGri@mail.ru>
2022-09-23 08:13:43 +0300

    Задача 10

---
+++ Vebinar2HomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int n = ReadInt("Введите трехзначное число: ");
+int a = n.ToString().Length;
+
+if (a < 3 || a > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(n));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a1)
+{
+    
+    int result = ((a1 / 10) % 10);
+    return result;
+}
