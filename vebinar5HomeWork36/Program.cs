5db2b45c5311f916e1d918a737bd8791ba3190c6
KanSlavanO <SlaviGri@mail.ru>
2022-10-06 20:43:24 +0300

    add work 36

---
+++ vebinar5HomeWork36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// Console.Clear();
+Console.WriteLine($"\nЗадача 36. Сумма элементов, стоящих на нечётных позициях:\n");
+Console.Write($"Введи количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
